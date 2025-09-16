a280b06094697243f667b216aa2f22d0a5a6947f
vudaian24 <vudaian12a7@gmail.com>
2025-09-16 10:02:52 +0700

    add: cms payload

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -53,23 +53,23 @@ export default buildConfig({
   },
   plugins: [
     payloadCloudPlugin(),
-    s3Storage({
-      collections: {
-        media: {
-          prefix: "media",
-          generateFileURL: ({ filename }) => {
-            return `https://${process.env.AWS_S3_BUCKET}.s3.${process.env.AWS_REGION || "ap-southeast-1"}.amazonaws.com/media/${filename}`;
-          },
-        },
-      },
-      bucket: process.env.AWS_S3_BUCKET || "",
-      config: {
-        credentials: {
-          accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
-          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
-        },
-        region: process.env.AWS_REGION || "ap-southeast-1",
-      },
-    }),
+    // s3Storage({
+    //   collections: {
+    //     media: {
+    //       prefix: "media",
+    //       generateFileURL: ({ filename }) => {
+    //         return `https://${process.env.AWS_S3_BUCKET}.s3.${process.env.AWS_REGION || "ap-southeast-1"}.amazonaws.com/media/${filename}`;
+    //       },
+    //     },
+    //   },
+    //   bucket: process.env.AWS_S3_BUCKET || "",
+    //   config: {
+    //     credentials: {
+    //       accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
+    //       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+    //     },
+    //     region: process.env.AWS_REGION || "ap-southeast-1",
+    //   },
+    // }),
   ],
 });
