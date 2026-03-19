476be37d33b32bbd8e51c3f53634d05773b40d70
vudaian24 <vudaian12a7@gmail.com>
2026-03-19 12:15:00 +0700

    Refactor code structure for improved readability and maintainability

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,12 @@
-import { withPayload } from "@payloadcms/next/withPayload";
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
-
 const nextConfig: NextConfig = {
-  /* config options here */
   output: "standalone",
   images: {
     dangerouslyAllowSVG: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "placehold.co",
-      },
-    ],
+    remotePatterns: [{ protocol: "https", hostname: "placehold.co" }],
   },
 };
-
-// Make sure you wrap your `nextConfig`
-// with the `withPayload` plugin
-export default withPayload(withNextIntl(nextConfig));
+export default withNextIntl(nextConfig);
